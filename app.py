8e653b661e0c0a87471465237831f0d1bfe97b29
sheriffdeenabu <sheriffdeenabdullah@gmail.com>
2025-05-02 13:58:29 +0530

    Intial importing and improving readability

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
 import streamlit as st
-st.write("Hi")
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Configuration
+st.set_page_config(page_title="Sri Lanka Food Security Dashboard", layout="wide")
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('suite-of-food-security-indicators_lka-cleaned.csv')
+    categories = {
+        'Average protein supply': 'Protein Supply',
+        'Gross domestic product per capita, PPP,': 'GDP per capita',
+        'Number of people undernourished': 'Undernourishment',
+        'Prevalence of severe food insecurity in the male adult population': 'Male Food Insecurity',
+        'Prevalence of severe food insecurity in the female adult population': 'Female Food Insecurity',
+        'Number of severely food insecure people': 'Severe Food Insecurity count',
+        'Political stability and absence of violence/terrorism': 'Political Stability',
+        'Percentage of population using safely managed drinking water services': 'Safe Water Access',
+        'Percentage of population using at least basic drinking water services': 'Basic Water Access',
+        'Number of obese adults': 'Obesity',
+        'Prevalence of low birthweight': 'Low Birthweight',
+        'Number of newborns with low birthweight': 'Low Birthweight Count'
+    }
+    df['Category'] = df['Item'].str.split('(').str[0].str.strip().replace(categories)
+    df['Unit'] = df['Unit'].str.replace('million No', 'in millions')
+    df['Year'] = df['Year'].astype(str)
+    return df
+
